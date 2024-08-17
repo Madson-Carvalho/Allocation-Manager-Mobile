7ed9fb339b60da193504e8117515e431b5c996c9
Madson-Carvalho <madsonraynnon@gmail.com>
2024-08-16 21:00:17 -0300

    Estruturação inicial do projeto.

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const SafeArea(child: Drawer()),
+      appBar: AppBar(
+        title: const Text("Allocation Manager"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
