ab906c636dafca0ba92129df141c6fa66e46c223
yurigrams <yurigrams@gmail.com>
2024-09-02 21:37:34 -0300

    Foi adicionado um bottomNavigatioBarItem, adicionado as logos e criado a pasta assets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,22 +6,49 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _MyHomePageState();
 }
+PageController pageController = PageController();
+int itemPosition = 0;
 
 class _MyHomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const SafeArea(child: Drawer()),
       appBar: AppBar(
         title: const Text("Allocation Manager"),
+        centerTitle: true,
+
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        leading: Image.asset('assets/logo_sem_texto-removebg-preview.png'),
+
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            itemPosition = index;
+          });
+          pageController.animateToPage(index, duration: Duration(microseconds: 300) , curve: Curves.easeIn);
+        },
+        currentIndex: itemPosition,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.groups), label: 'Colaboradores'),
+          BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: 'Gant')
+        ],
       ),
+      body: PageView(
+        controller: pageController,
+        children: [
+          Container(color: Colors.red,),
+          Container(color: Colors.green,),
+          Container(color: Colors.yellow,),
+        ],
+      ),
+      // body: const Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[],
+      //   ),
+      // ),
     );
   }
 }
