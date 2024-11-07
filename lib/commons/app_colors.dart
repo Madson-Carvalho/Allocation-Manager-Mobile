b79bf55eceb533b4fb743690d504fbdb86978a0b
yurigrams <yurigrams@gmail.com>
2024-11-07 20:29:37 -0300

    Deixando a pagina de cadastro de funcionarios responsiva e fazendo alreações para ficar de acordo com o planejamento

--- lib/commons/app_colors.dart
+++ lib/commons/app_colors.dart
@@ -8,4 +8,6 @@ class AppColors {
   static const Color red = Color(0xFFFF0000);
   static const Color ligthBlue = Color(0xFF6DB6FE);
   static const Color seanBlue = Color(0xFF0068FF);
+  static const Color primaryColorSofty = Color(0x812A63A9);
+  static const Color black = Color(0xFF000000);
 }
